020509b2de188a4be14cf7465db24f0f2c6988e6
vinifred1003 <viniciusfrederico1003@gmail.com>
2023-09-28 17:54:05 -0300

    first commit

---
+++ View/loginScreen.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import *
+
+class LoginScreen():
+  
+  def __init__(self):
+    self.root = tk.Tk()
+    self.run
+    self.root.mainloop()
+  
+  def run(self):
+    self.root.title("Login")
+    self.root.geometry("1000x1200")
+    self.root.configure(background="gray")
+    self.root.resizable(True, True)
+
